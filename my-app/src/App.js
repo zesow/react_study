1ab5d4bf8e6350523fbdccefd10cba5bada9d555
zesow <anstkd07@gmail.com>
2018-11-06 15:56:03 +0900

    components practice(movie)

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,32 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie';
 
+// 각각에게 다른 제목 부여. 이게 각각의 props 임.
+const movieTitles = [
+  "열연",
+  "냉연",
+  "순환품",
+  "STS"
+]
+
+const movieImages = [
+  "https://marketing.adobe.com/resources/help/ko_KR/sc/user/graphics/graph_trend_line.png",
+  "https://marketing.adobe.com/resources/help/ko_KR/sc/user/graphics/graph_smooth_line.png",
+  "https://marketing.adobe.com/resources/help/ko_KR/sc/user/graphics/graph_stacked_area.png",
+  "https://marketing.adobe.com/resources/help/ko_KR/sc/user/graphics/graph_stacked_horizontal.png"
+]
+
 class App extends Component {
   render() {
     // 이 컴포넌트가 나에게 보여주는 것이 무엇인지를 정의.
     return (
       // 자바스크립트 안에 html = jsx
       <div className="App">
-        <Movie /> 입니다.
-        
+        <Movie title={movieTitles[0]} poster={movieImages[0]}/>
+        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
+        <Movie title={movieTitles[2]} poster={movieImages[2]}/>
+        <Movie title={movieTitles[3]} poster={movieImages[3]}/> 
+
       </div>
     );
   }
--- my-app/src/Movie.js
+++ my-app/src/Movie.js
@@ -3,10 +3,25 @@ import './Movie.css';
 
 class Movie extends Component{
     render(){
+        console.log(this.props);
         return(
-        <h1>hello this is a movie</h1>
+        <div>
+            <MoviePoseter poster={this.props.poster} />
+            {/* 각각에게 맞는 제목들. movie.js 에서 가져옴. 
+            괄호치는거 항상 주의
+            */}
+            <h1>{this.props.title}</h1>
+        </div>
         )
     }
 }
 
+class MoviePoseter extends Component{
+    render(){
+        console.log(this.props)
+        return(
+            <img src={this.props.poster} />
+        )
+    }
+}
 export default Movie
