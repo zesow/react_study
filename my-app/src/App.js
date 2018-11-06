f171e1845ddc5e2f8e1eaf89191a3c4ed6fa0f59
zesow <anstkd07@gmail.com>
2018-11-06 18:46:25 +0900

    map

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,31 +4,38 @@ import './App.css';
 import Movie from './Movie';
 
 // 각각에게 다른 제목 부여. 이게 각각의 props 임.
-const movieTitles = [
-  "열연",
-  "냉연",
-  "순환품",
-  "STS"
-]
 
-const movieImages = [
-  "https://marketing.adobe.com/resources/help/ko_KR/sc/user/graphics/graph_trend_line.png",
-  "https://marketing.adobe.com/resources/help/ko_KR/sc/user/graphics/graph_smooth_line.png",
-  "https://marketing.adobe.com/resources/help/ko_KR/sc/user/graphics/graph_stacked_area.png",
-  "https://marketing.adobe.com/resources/help/ko_KR/sc/user/graphics/graph_stacked_horizontal.png"
+const movies = [
+  {
+    title: "열연",
+    poster: "https://marketing.adobe.com/resources/help/ko_KR/sc/user/graphics/graph_trend_line.png"
+  },
+  {
+    title: "냉연",
+    poster: "https://marketing.adobe.com/resources/help/ko_KR/sc/user/graphics/graph_smooth_line.png"
+  },
+  {
+    title: "순환품",
+    poster: "https://marketing.adobe.com/resources/help/ko_KR/sc/user/graphics/graph_stacked_area.png"
+  },
+  {
+    title: "STS",
+    poster: "https://marketing.adobe.com/resources/help/ko_KR/sc/user/graphics/graph_stacked_horizontal.png"
+  }
 ]
-
 class App extends Component {
   render() {
     // 이 컴포넌트가 나에게 보여주는 것이 무엇인지를 정의.
     return (
       // 자바스크립트 안에 html = jsx
       <div className="App">
-        <Movie title={movieTitles[0]} poster={movieImages[0]}/>
-        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
-        <Movie title={movieTitles[2]} poster={movieImages[2]}/>
-        <Movie title={movieTitles[3]} poster={movieImages[3]}/> 
-
+      {/* map 기능. 
+        map 은 array를 만듦. 다른 array의 요소를 포함하는 값.
+        array를 이용해서 컴포넌트들을 생성해준다.
+      */}
+        {movies.map(movie => {
+          return <Movie title={movie.title} poster={movie.poster} />
+        })}
       </div>
     );
   }
