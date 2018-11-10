7f9cfc734cab55c260d5e6aef5478f0098609d6f
zesow <anstkd07@gmail.com>
2018-11-11 01:08:16 +0900

    async,await

--- README.md
+++ README.md
@@ -776,3 +776,66 @@ class App extends Component {
 export default App;
 
 ```
+
+#17. async, await
+
+```
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Movie from './Movie';
+
+
+class App extends Component {
+
+  state = {
+
+  }
+
+  componentDidMount(){
+    // didmount 의 크기를 줄임. function 별로 크기를 줄이는게 좋은 것.
+    this._getMovies(); 
+  }
+
+  // 비동기. 이전 라인의 작업이 끝날때까지 기다리는 게 아닐 때 사용.
+  _getMovies = async () => {
+    // callApi 를 await 에서. callApi가 끝나기를 기다렸다가 그 리턴값을 movies에 넣음.
+    const movies = await this._callApi()
+    // await 를 했기 때문에 callApi가 값을 다 받아온 다음에 setState 가 실행된다.
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    // then 이 많아진다 = 콜백 지옥. 해결책이 필요함.
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = 'https://yts.ag/api/v2/list_movies.json?sort_by=rating'
+    return fetch(proxyurl + url)
+    .then(response => response.json())
+    // 화살표 함수 = 화살표에 return 이 내포되어 있음.
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+  // 데이터가 없을 때 '로딩' 을 띄우고, 있으면 영화정보가 보이도록 함.
+  // 이게 무슨 뜻일까??
+  // map을 이용해서 movie 들을 출력.
+  _renderMovies = () => {
+    const movies = this.state.movies.map(movie => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
+    })
+    return movies
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.movies ? this._renderMovies() : 'Loading'}
+      </div>
+    );
+  }
+}
+
+export default App;
+
+```
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,15 +11,28 @@ class App extends Component {
   }
 
   componentDidMount(){
-    // fetch가 끝나면 then을 해라. 그런데 에러가 발생하면 err를 실행해라.
-    // json 으로  바꿈.
-    // cors 에러 해결. proxyurl 을 추가해준다. https://stackoverflow.com/questions/43871637/no-access-control-allow-origin-header-is-present-on-the-requested-resource-whe
-    // api 받아서 콘솔에 출력 성공.
+    // didmount 의 크기를 줄임. function 별로 크기를 줄이는게 좋은 것.
+    this._getMovies(); 
+  }
+
+  // 비동기. 이전 라인의 작업이 끝날때까지 기다리는 게 아닐 때 사용.
+  _getMovies = async () => {
+    // callApi 를 await 에서. callApi가 끝나기를 기다렸다가 그 리턴값을 movies에 넣음.
+    const movies = await this._callApi()
+    // await 를 했기 때문에 callApi가 값을 다 받아온 다음에 setState 가 실행된다.
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    // then 이 많아진다 = 콜백 지옥. 해결책이 필요함.
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
     const url = 'https://yts.ag/api/v2/list_movies.json?sort_by=rating'
-    fetch(proxyurl + url)
+    return fetch(proxyurl + url)
     .then(response => response.json())
-    .then(json => console.log(json))
+    // 화살표 함수 = 화살표에 return 이 내포되어 있음.
+    .then(json => json.data.movies)
     .catch(err => console.log(err))
   }
 
@@ -27,8 +40,8 @@ class App extends Component {
   // 이게 무슨 뜻일까??
   // map을 이용해서 movie 들을 출력.
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map(movie => {
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
     })
     return movies
   }
